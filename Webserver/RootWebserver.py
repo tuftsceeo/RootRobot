8916a78f60bc4422c44df0bc394a8546d7b3909e
Daniel McGinn <danmcginn@comcast.net>
2019-07-26 11:34:45 -0400

    Update RootWebserver.py

--- Webserver/RootWebserver.py
+++ Webserver/RootWebserver.py
@@ -111,19 +111,19 @@ class RootDevice(gatt.Device):
 #        print ("Messages from Root:")
         if message[0] == 4: 
             type = "Color Sensor"; 
-            sensorData = sensorData + datetime.datetime.now().time() + ': Color Sensor Triggered<br>'
+            sensorData = sensorData + str(datetime.datetime.now().time()) + ': Color Sensor Triggered<br>'
         if message[0] == 12: 
             type = "Bumper"; 
-            sensorData = sensorData + datetime.datetime.now().time() + ': Bumper Triggered<br>'
+            sensorData = sensorData + str(datetime.datetime.now().time()) + ': Bumper Triggered<br>'
         if message[0] == 13: 
             type = "Light Sensor"; 
-            sensorData = sensorData + datetime.datetime.now().time() + ': Light Sensor Triggered<br>'
+            sensorData = sensorData + str(datetime.datetime.now().time()) + ': Light Sensor Triggered<br>'
         if message[0] == 17: 
             type = "Touch Sensor"; 
-            sensorData = sensorData + datetime.datetime.now().time() + ': Touch Sensor Triggered<br>'
+            sensorData = sensorData + str(datetime.datetime.now().time()) + ': Touch Sensor Triggered<br>'
         if message[0] == 20: 
             type = "Cliff Sensor"; 
-            sensorData = sensorData + datetime.datetime.now().time() + ': Cliff Sensor Triggered<br>'
+            sensorData = sensorData + str(datetime.datetime.now().time()) + ': Cliff Sensor Triggered<br>'
 
         print(type, message)
         return sensorData
